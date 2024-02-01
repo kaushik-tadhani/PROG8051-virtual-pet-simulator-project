500706b6268abaeb2d4da5fc1da08da346e18a91
kaushik-tadhani <tadkaushik@gmail.com>
2024-02-01 14:27:42 -0500

    user input for the pet type and name with welcome message

--- Program.cs
+++ Program.cs
@@ -8,7 +8,21 @@ namespace PROG8051_virtual_pet_simulator_project
     {
         static void Main(string[] args)
         {
-            
+            string petType;
+            string petName;
+
+            Console.WriteLine("Please choose a type of pet: Cat, Dog or Rabbit etc..");
+
+            Console.Write("User Input: ");
+            petType = Console.ReadLine();
+
+
+            Console.WriteLine($"\nYou've chosen a {petType}. What would you like to name your pet?");
+
+            Console.Write("\nUser input: ");
+            petName = Console.ReadLine();
+
+            Console.WriteLine($"\nWelcome, {petName}! Let's take good care of him. \n");
 
         }
     }
